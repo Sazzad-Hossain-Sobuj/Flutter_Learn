6dcb0c177454ff7eab32056b4ef1854ad22614df
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-12-05 23:19:08 +0600

    module 12 assignment task-manager-project

---
+++ TASK-MANAGER-RAFAT/task_manager_project_rafat/lib/ui/screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../controllers/auth_controller.dart';
+import '../widgets/body_background.dart';
+import 'login_screen.dart';
+import 'main_bottom_nav_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    goToLogin();
+  }
+
+  Future<void> goToLogin() async {
+    final bool isLoggedIn = await AuthController.checkAuthState();
+
+    Future.delayed(const Duration(seconds: 2)).then((value) {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+              builder: (context) => isLoggedIn
+                  ? const MainBottomNavScreen()
+                  : const LoginScreen()),
+              (route) => false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BodyBackground(
+          child: Center(
+            child: SvgPicture.asset(
+              'assets/images/app_logo.svg',
+              width: 120,
+            ),
+          ),
+        )
+    );
+  }
+}
